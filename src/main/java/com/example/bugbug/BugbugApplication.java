a3e8260c7656cac3f573d0f99cc942cf446307d6
Sodai-Komichi <83051052+Aso2101122@users.noreply.github.com>
2022-11-09 13:07:56 +0900

    初期コミット

---
+++ src/main/java/com/example/bugbug/BugbugApplication.java
@@ -0,0 +1,13 @@
+package com.example.bugbug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugbugApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BugbugApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bugbug/BugbugApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bugbug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugbugApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
