dcc4540f9bfae6c50579806ca4a0bc4562f01fde
Sodai-Komichi <83051052+Aso2101122@users.noreply.github.com>
2022-12-14 12:08:49 +0900

    ログイン情報をフロント側に引き渡す、ログインテキストボックス文字数制限

--- src/main/java/com/example/bugbug/form/LoginForm.java
+++ src/main/java/com/example/bugbug/form/LoginForm.java
@@ -3,11 +3,14 @@ package com.example.bugbug.form;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Data
 public class LoginForm {
 	@NotBlank
+	@Size(max = 128)
 	private String mail;
 	@NotBlank
+	@Size(max = 128)
 	private String password;
 }
--- src/main/java/com/example/bugbug/form/UserRegisterForm.java
+++ src/main/java/com/example/bugbug/form/UserRegisterForm.java
@@ -12,9 +12,9 @@ public class UserRegisterForm {
 	@Email
 	private String mail;
 	@NotBlank
-	@Size(min = 6, max = 128)
+	@Size(min = 2, max = 128)
 	private String name;
-	@Pattern(regexp="(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-z0-9.?/-]")
+	@Pattern(regexp="(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-z0-9./?-]{6,128}")
 	@Size(min = 6, max = 128)
 	private String password;
 	private String confirmPassword;//確認用パスワード
