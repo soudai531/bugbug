e0de75d6c91c1bebc3f24b86fb5f9b262a581281
itoito <2001003@s.asojuku.ac.jp>
2022-12-16 22:19:57 +0900

    Update index.js

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,7 +1,9 @@
 "use strict";
 
-import { setHeaderScroll } from "./module/page/index/pageTop";
+import { removeHeaderNavBtn } from "./module/header";
+import { setHeaderScroll} from "./module/page/index/pageTop";
 import { setRecipeSearchForm } from "./module/recipeSearchForm";
+import { setConfirmDialog } from "./module/confirm";
 import { setRecipeHref } from "./module/recipeHref";
 import { setRecipeHeartBtn } from "./module/recipeHeartBtn";
 import { setRecipeImageFilterBtn } from "./module/recipeImageFilterBtn";
@@ -9,7 +11,7 @@ import { setRecipeImageFilterBtn } from "./module/recipeImageFilterBtn";
 setRecipeSearchForm();
 
 /* PAGE : index */
-if (["/bugbug/", "/bugbug/index.html"].includes(location.pathname)) {
+if (["", "/", "/index", "/index.html"].includes(location.pathname)) {
   // SET -> ヘッダースクロール処理
   setHeaderScroll();
   // SET -> レシピクリック時遷移処理
@@ -18,4 +20,18 @@ if (["/bugbug/", "/bugbug/index.html"].includes(location.pathname)) {
   setRecipeHeartBtn();
   // SET -> レシピ画像フィルターボタンクリック処理
   setRecipeImageFilterBtn();
+}
+
+/* PAGE : login */
+if (["/login/form", "/login"].includes(location.pathname)) {
+  // RUN -> ヘッダーナビボタン非表示
+  removeHeaderNavBtn();
+}
+
+/* PAGE : signup */
+if (["/signup/form", "/signup"].includes(location.pathname)) {
+  // RUN -> ヘッダーナビボタン非表示
+  removeHeaderNavBtn();
+  // SET -> 登録時確認ダイアログ処理
+  setConfirmDialog(".signup__form__btn", "この内容で登録しますか");
 }
