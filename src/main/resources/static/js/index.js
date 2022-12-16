43cc3bf11bf6c304e6cf2b8d4d3c797efeefae8a
itoito <2001003@s.asojuku.ac.jp>
2022-12-16 12:36:13 +0900

    Update index.js

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -2,14 +2,16 @@
 
 import { setHeaderScroll } from "./module/page/index/pageTop";
 import { setRecipeSearchForm } from "./module/recipeSearchForm";
+import { setConfirmDialog } from "./module/confirm";
 import { setRecipeHref } from "./module/recipeHref";
 import { setRecipeHeartBtn } from "./module/recipeHeartBtn";
 import { setRecipeImageFilterBtn } from "./module/recipeImageFilterBtn";
+import { setRecipeHowtoImage } from "./module/page/recipe/pageRecipe";
 
 setRecipeSearchForm();
 
 /* PAGE : index */
-if (["/bugbug/", "/bugbug/index.html"].includes(location.pathname)) {
+if (["", "/", "/index", "/index.html"].includes(location.pathname)) {
   // SET -> ヘッダースクロール処理
   setHeaderScroll();
   // SET -> レシピクリック時遷移処理
@@ -18,4 +20,16 @@ if (["/bugbug/", "/bugbug/index.html"].includes(location.pathname)) {
   setRecipeHeartBtn();
   // SET -> レシピ画像フィルターボタンクリック処理
   setRecipeImageFilterBtn();
+}
+
+/* PAGE : recipe */
+if (/^\/recipes\/[0-9]*/.test(location.pathname)) {
+  // SET -> 作り方画像処理
+  setRecipeHowtoImage();
+  // SET - > レシピハートボタンクリック処理
+  setRecipeHeartBtn();
+  // SET -> レシピ画像フィルターボタンクリック処理
+  setRecipeImageFilterBtn();
+  // SET -> 削除時確認ダイアログ処理
+  setConfirmDialog(".recipeDetail__recipeAction__btns__btn--delete", "削除しますか");
 }
